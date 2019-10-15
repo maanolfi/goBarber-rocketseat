9f05a1cb1473814d1beb848119e86b0b75507481
Marco Antonio <marco.oliveira92@hotmail.com>
2019-10-15 15:49:24 -0300

    Create model-user

--- src/config/database.js
+++ src/config/database.js
@@ -1,12 +1,13 @@
 module.exports = {
-  dialetc: 'postgres',
-  host: 'localhost',
+  dialect: 'postgres',
+  host: '127.0.0.1',
   username: 'docker',
   password: 'docker',
   database: 'gobarber',
+  operatorAliases: false,
   define: {
     timestamps: true,
     underscored: true,
-    underscoredAll: true
-  }
-}
+    underscoredAll: true,
+  },
+};
--- src/database/migrations/20191015132222-create-users.js
+++ src/database/migrations/20191015132222-create-users.js
@@ -1,17 +1,42 @@
-'use strict';
-
 module.exports = {
   up: (queryInterface, Sequelize) => {
-      return queryInterface.createTable('users', {
-        id: Sequelize.INTEGER
-
-
-      });
-
+    return queryInterface.createTable('users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      name: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      email: {
+        allowNull: false,
+        unique: true,
+        type: Sequelize.STRING,
+      },
+      password_hash: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      provider: {
+        allowNull: false,
+        defaultValue: false,
+        type: Sequelize.BOOLEAN,
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
   },
 
-  down: (queryInterface) => {
-      return queryInterface.dropTable('users');
-
-  }
+  down: queryInterface => {
+    return queryInterface.dropTable('users');
+  },
 };
