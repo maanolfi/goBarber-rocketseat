09370c36fef8346d849f43d3eff25061be840387
Marco Antonio <marco.oliveira92@hotmail.com>
2019-10-15 18:46:14 -0300

    Update of users

---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: 'gre4g9e49t46e4gf8rew49',
+  expiresIn: '2d',
+};
--- src/routes.js
+++ src/routes.js
@@ -1,15 +1,16 @@
 import { Router } from 'express';
-import User from './app/models/User';
+
+import UserController from './app/controllers/UserController';
+import SessionController from './app/controllers/SessionController';
+
+import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
 
-routes.get('/', async (req, res) => {
-  const user = await User.create({
-    name: 'Marco',
-    email: 'marco@gmail.com',
-    password_hash: '123456',
-  });
-  return res.json(user);
-});
+routes.post('/users', UserController.store);
+routes.post('/session', SessionController.store);
+
+routes.use(authMiddleware);
+routes.put('/users', UserController.update);
 
 export default routes;
