fd2b56fd4759edb28f7f4f912a9c2419fd021576
Marco Antonio <marco.oliveira92@hotmail.com>
2019-11-06 17:44:55 -0300

    choic-cancel-appointments

--- src/routes.js
+++ src/routes.js
@@ -22,6 +22,7 @@ routes.put('/users', UserController.update);
 
 routes.get('/appointments', AppointmentController.index);
 routes.post('/appointments', AppointmentController.store);
+routes.delete('/appointments/:id', AppointmentController.delete);
 
 routes.get('shedule', ScheduleController.index);
 
