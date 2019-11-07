78975d7ab6524e0790b3e0f7cf4b519e7e5eaa4d
Marco Antonio <marco.oliveira92@hotmail.com>
2019-11-07 11:38:17 -0300

    feat-config-nodemailer-hbs

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '00288152799180',
+    pass: 'a95c39248f1152',
+  },
+  defaut: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
